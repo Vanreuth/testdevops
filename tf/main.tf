0d50e8826c99dafc7fb2966734cb784d57fe9666
Vanreuth <hengvanreuth.17@gmail.com>
2025-08-17 10:55:17 +0700

    update

--- tf/main.tf
+++ tf/main.tf
@@ -7,17 +7,6 @@ variable "aws_region" {
   type        = string
 }
 
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-  sensitive   = true
-}
 resource "aws_security_group" "sg_1" {
   name = "kimang-security"
 
