ac9926311843195e2e143dc22dfe114e678581a2
Vanreuth <hengvanreuth.17@gmail.com>
2025-08-17 17:16:36 +0700

    done

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch(err => console.error("❌ MongoDB connection error:", err));
+// // MongoDB connection
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log("✅ MongoDB connected"))
+//   .catch(err => console.error("❌ MongoDB connection error:", err));
 
 app.get("/", (req, res) => res.send("Hello from Node.js + MongoDB on AWS!"));
 
